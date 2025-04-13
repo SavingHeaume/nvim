4c3facb91087be51c39624856ba44aecf947ae4b
meng <1792766874@qq.com>
2025-04-13 11:09:11 +0800

    feat: 添加适用wsl的剪切版

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -14,3 +14,18 @@ o.softtabstop = 2
 
 o.number = true
 o.numberwidth = 2
+
+g.clipboard = {
+  name = 'WslClipboard',
+  copy = {
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe',
+  },
+  paste = {
+    ['+'] =
+    'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['*'] =
+    'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}

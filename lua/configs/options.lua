5d6f4bef7524b4510c6f2ce2b7c02803fb48638a
meng <1792766874@qq.com>
2025-04-24 16:26:12 +0800

    fix: 修改clip为win32yank

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -37,16 +37,14 @@ o.ruler = false
 opt.shortmess:append "sI"
 
 g.clipboard = {
-  name = 'WslClipboard',
+  name = 'win32yank',
   copy = {
-    ['+'] = 'clip.exe',
-    ['*'] = 'clip.exe',
+    ['+'] = 'win32yank.exe -i --crlf',
+    ['*'] = 'win32yank.exe -i --crlf',
   },
   paste = {
-    ['+'] =
-    'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    ['*'] =
-    'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['+'] = 'win32yank.exe -o --lf',
+    ['*'] = 'win32yank.exe -o --lf',
   },
   cache_enabled = 0,
 }

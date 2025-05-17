aceca4bcbb99e21a857e1280cffd312bc227e7d1
meng <1792766874@qq.com>
2025-05-17 22:39:31 +0800

    feat: 添加fittencode，去除部分注释

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,9 @@
+local M = {
+  {
+    'luozhiya/fittencode.nvim',
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    opts = {},
+  },
+}
+
+return M
--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -39,13 +39,10 @@ local M = {
     branch = "v3.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
-      -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
     },
-    lazy = false, -- neo-tree will lazily load itself
-    ---@module "neo-tree"
-    ---@type neotree.Config?
+    lazy = false,
     opts = {
       window = {
         width = 30
